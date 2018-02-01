10d4ffd8c519d65372545c34ecbb6ae88da5b929
Tatiana <tatimagdalena@gmail.com>
2018-02-01 20:54:07 -0200

    Add registration view controller files

---
+++ DataValitation-MVP/RegistrationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationViewController.swift
+//  DataValitation-MVP
+//
+//  Created by Tatiana Magdalena on 01/02/18.
+//  Copyright © 2018 Tatiana Magdalena. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
