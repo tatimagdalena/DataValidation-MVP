d810ab02c0277ecac4f9f6a5c664e4d01c2f680e
Tatiana <tatimagdalena@gmail.com>
2018-02-02 09:46:27 -0200

    Add name validator tests

---
+++ DataValitation-MVP/NameValidator.swift
@@ -0,0 +1,19 @@
+//
+//  NameValidator.swift
+//  DataValitation-MVP
+//
+//  Created by Tatiana Magdalena on 02/02/18.
+//  Copyright © 2018 Tatiana Magdalena. All rights reserved.
+//
+
+import Foundation
+
+class NameValidator {
+    
+    func validate(name: String) -> Bool {
+        
+        
+        return false
+    }
+    
+}
