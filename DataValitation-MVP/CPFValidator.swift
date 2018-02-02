a247a170a74f3781b19b331b30d4fe9f9c666b43
Tatiana Magdalena <tmagdalena@stone.com.br>
2018-02-02 15:15:09 -0200

    Add cpf validator tests

---
+++ DataValitation-MVP/CPFValidator.swift
@@ -0,0 +1,9 @@
+//
+//  CPFValidator.swift
+//  DataValitation-MVP
+//
+//  Created by Tatiana Magdalena on 02/02/18.
+//  Copyright © 2018 Tatiana Magdalena. All rights reserved.
+//
+
+import Foundation
