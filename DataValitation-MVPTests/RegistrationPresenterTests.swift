53d2bd08ee5ebc3b918e5c86803b5235cc14bedc
Tatiana <tatimagdalena@gmail.com>
2018-02-04 20:31:30 -0200

    Add presenter tests

--- DataValitation-MVPTests/RegistrationPresenterTests.swift
+++ DataValitation-MVPTests/RegistrationPresenterTests.swift
@@ -13,41 +13,104 @@ class RegistrationPresenterTests: XCTestCase {
     
     class MockValidationView: ValidationView {
         
-        var invalidCPFCallCount = 0
-        var invalidNameCallCount = 0
-        var invalidEmailCallCount = 0
-        var readyToValidateCallCount = 0
+        static var invalidCPFCallCount = 0
+        static var invalidNameCallCount = 0
+        static var invalidEmailCallCount = 0
+        static var readyToValidateCallCount = 0
+        
+        var presenter: RegistrationPresenter?
         
         func onCPFInvalid() {
-            invalidCPFCallCount += 1
+            MockValidationView.invalidCPFCallCount += 1
         }
         
         func onNameInvalid() {
-            invalidNameCallCount += 1
+            MockValidationView.invalidNameCallCount += 1
         }
         
         func onEmailInvalid() {
-            invalidEmailCallCount += 1
+            MockValidationView.invalidEmailCallCount += 1
         }
         
         func onReadyToValidate() {
-            readyToValidateCallCount += 1
+            MockValidationView.readyToValidateCallCount += 1
         }
     }
     
     var presenterUnderTest: RegistrationPresenter!
+    var mockView: MockValidationView!
     
     override func setUp() {
         super.setUp()
-        presenterUnderTest = RegistrationPresenter(view: MockValidationView(),
+        
+        mockView = MockValidationView()
+        presenterUnderTest = RegistrationPresenter(view: mockView,
                                                    nameValidator: NameValidator(),
                                                    emailValidator: EmailValidator(),
                                                    cpfValidator: CPFValidator())
+        mockView.presenter = presenterUnderTest
     }
     
     override func tearDown() {
+        mockView = nil
         presenterUnderTest = nil
         super.tearDown()
+    }
+    
+    func testInvalidNameChange() {
+        
+        // given
+        let name = "Ab C"
+        
+        //when
+        presenterUnderTest.userNameChanged(newName: name)
+        
+        //then
+        XCTAssertEqual(MockValidationView.invalidNameCallCount, 1)
+        
+    }
+    
+    func testInvalidEmailChange() {
+        
+        // given
+        let email = "mail.domain.com"
+        
+        //when
+        presenterUnderTest.userEmailChanged(newEmail: email)
+        
+        //then
+        XCTAssertEqual(MockValidationView.invalidEmailCallCount, 1)
+        
+    }
+    
+    func testInvalidCPFChange() {
+        
+        // given
+        let cpf = "75839274637"
+        
+        //when
+        presenterUnderTest.userCPFChanged(newDocument: cpf)
+        
+        //then
+        XCTAssertEqual(MockValidationView.invalidCPFCallCount, 1)
+        
+    }
+    
+    func testAllInputsAreValid() {
+        
+        // given
+        let name = "Fulano Silva"
+        let cpf = "54644757969"
+        let email = "mail@domain.com"
+        
+        //when
+        presenterUnderTest.userNameChanged(newName: name)
+        presenterUnderTest.userEmailChanged(newEmail: email)
+        presenterUnderTest.userCPFChanged(newDocument: cpf)
+        
+        //then
+        XCTAssertEqual(MockValidationView.readyToValidateCallCount, 1)
+        
     }
     
 }
